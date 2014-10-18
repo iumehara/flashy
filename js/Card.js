cc9bb715640f689dcb350210e8dda57d541d8f1d
terimiyaki <wackoli123@yahoo.com>
2014-10-18 14:50:01 -0400

    forgot to save the file like a noob. Card.js

--- js/Card.js
+++ js/Card.js
@@ -1,3 +1,14 @@
 function Card(){
-	
+	$(document).ready(function(){
+				$(".top").addClass("show");
+				$(".view").click(function(){
+					if($(".top").hasClass("show")){
+						$(".bottom").addClass("show");
+						$(".top").removeClass("show");
+					} else {
+						$(".top").addClass("show");
+						$(".bottom").removeClass("show");
+					}
+				})
+			});
 }
