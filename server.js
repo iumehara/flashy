e8ef4399256ad3cc8f28664d3b155f3f432a2e1b
Jesse Lee <mienaikoe@gmail.com>
2014-10-18 16:28:34 -0400

    fixing node

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 var static = require('node-static');
+var util = require('util');
 
 var publicDirectory = new static.Server('./public');
 
-require('http').createServer(function (request, response) {
-    request.addListener('end', function () {
-        publicDirectory.serve(request, response);
-    }).resume();
+
+require('http').createServer(function(request, response) {
+	request.addListener('end', function() {
+		publicDirectory.serve(request, response, function(err, result) {
+			if (err) {
+				console.error('Error serving %s - %s', request.url, err.message);
+				if (err.status === 404 || err.status === 500) {
+					publicDirectory.serveFile(util.format('/%d.html', err.status), err.status, {}, request, response);
+				} else {
+					response.writeHead(err.status, err.headers);
+					response.end();
+				}
+			} else {
+				console.log('%s - %s', request.url, response.message);
+			}
+		});
+	}).resume();
 }).listen(3000);
