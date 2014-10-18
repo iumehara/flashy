d61383272fcf40a65463e37d9188f6d0e40a76b2
Jesse Lee <mienaikoe@gmail.com>
2014-10-18 14:18:00 -0400

    Adding jquery

--- js/Flashy.js
+++ js/Flashy.js
@@ -1,3 +1,31 @@
-function Flashy(){
+function Flashy( type ){
+	this.type = type;
+	
+	function makeTools(body){
+		$("<div/>",{"class":"button back"}).click(function(ev){
+			window.location = "/";
+		}).appendTo(body);
+		
+		if( type === "creator" ){
+			$("<div/>",{"class":"button menu"}).click(function(ev){
+				window.location = "/";
+			}).appendTo(body);
+		} else {
+			$("<div/>",{"class":"button favorite"}).click(function(ev){
+				window.location = "/";
+			}).appendTo(body);
+		}
+		
+	}
+	
+	
+	
+	
+	
+	makeTools(body);
+	// Make back tool
+	
+	
+	
 	
 }
